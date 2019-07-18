417a2e4f4c645c9382c5e34f73ac57c954b5255f
Ossi Myllymäki <omyllymaki@gmail.com>
2019-07-18 15:30:08 +0300

    Add base test case

---
+++ tests/base_test_case.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+
+import numpy as np
+from numpy.testing import assert_almost_equal
+
+from file_io import load_pickle_file
+from utils import calculate_signal
+
+
+class BaseTestCase(unittest.TestCase):
+    method = None
+
+    def setUp(self):
+        root_path = os.path.abspath(os.path.dirname(__file__))
+        file_path = os.path.join(root_path, 'data', 'pure_components.p')
+        self.pure_components = load_pickle_file(file_path)
+        self.x = np.arange(0, 100)
+        self.contributions = np.array([1, 3, 5])
+        self.mixture_signal = calculate_signal(self.contributions, self.pure_components)
+
+    def run_test(self, signal, decimal: int = 2):
+        actual, _ = self.method(self.x,
+                                signal,
+                                self.pure_components)
+        expected = self.contributions
+        assert_almost_equal(actual, expected, decimal=decimal)
