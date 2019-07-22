6085073b06f6302d88d02a5c92e00269e16e4f2c
Ossi Myllymäki <omyllymaki@gmail.com>
2019-07-22 10:26:41 +0300

    Add GridGNSolver and change file names

--- fit_with_x_axis_correction/tests/test_ea_solver.py
+++ fit_with_x_axis_correction/tests/test_ea_solver.py
@@ -1,21 +1,19 @@
-import os
 import timeit
 from functools import partial
 
-from file_io import load_pickle_file
-from fit_with_x_axis_correction.GN_solver import GNSolver
+from fit_with_x_axis_correction.ea_solver import EASolver
 from fit_with_x_axis_correction.common import interpolate_signal
-from fit_with_x_axis_correction.correction_models import quadratic_correction
+from fit_with_x_axis_correction.correction_models import linear_correction
 from fit_with_x_axis_correction.tests.base_test_case import BaseTestCase
 
 
-class TestGNSolver(BaseTestCase):
-
+class TestEASolver(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.solver = GNSolver(x=self.x,
+        self.solver = EASolver(x=self.x,
                                pure_components=self.pure_components,
-                               correction_model=quadratic_correction)
+                               correction_model=linear_correction,
+                               rsme_threshold=0.1)
 
     def test_no_x_axis_errors_should_pass(self) -> None:
         self.run_test(self.mixture_signal)
@@ -35,23 +33,9 @@ class TestGNSolver(BaseTestCase):
         signal = interpolate_signal(self.mixture_signal, self.x, x_distorted, 0, 0)
         self.run_test(signal)
 
-    def test_quadratic_error_should_pass(self) -> None:
-        x_distorted = 0.01 * self.x ** 2 + 1.01 * self.x + 2
-        signal = interpolate_signal(self.mixture_signal, self.x, x_distorted, 0, 0)
-        self.run_test(signal)
-
     def test_speed(self):
         x_distorted = 1.01 * self.x - 2
         signal = interpolate_signal(self.mixture_signal, self.x, x_distorted, 0, 0)
         f = partial(self.solver.solve, signal=signal)
-        time = timeit.timeit(f, number=50)
-        self.assertLess(time, 1)
-
-    def test_with_random_concentrations_and_random_linear_x_errors_should_pass(self):
-        root_path = os.path.abspath(os.path.dirname(__file__))
-        file_path = os.path.join(root_path, 'data', 'mixtures.p')
-        mixture_data = load_pickle_file(file_path)
-        for item in mixture_data:
-            self.contributions = item['contributions']
-            signal = item['signal']
-            self.run_test(signal, decimal=1)
+        time = timeit.timeit(f, number=20)
+        self.assertLess(time, 10)
--- fit_with_x_axis_correction/tests/test_gn_solver.py
+++ fit_with_x_axis_correction/tests/test_gn_solver.py
@@ -1,19 +1,21 @@
+import os
 import timeit
 from functools import partial
 
-from fit_with_x_axis_correction.EA_solver import EASolver
+from file_io import load_pickle_file
+from fit_with_x_axis_correction.gn_solver import GNSolver
 from fit_with_x_axis_correction.common import interpolate_signal
-from fit_with_x_axis_correction.correction_models import linear_correction
+from fit_with_x_axis_correction.correction_models import quadratic_correction
 from fit_with_x_axis_correction.tests.base_test_case import BaseTestCase
 
 
-class TestEA(BaseTestCase):
+class TestGNSolver(BaseTestCase):
+
     def setUp(self):
         super().setUp()
-        self.solver = EASolver(x=self.x,
+        self.solver = GNSolver(x=self.x,
                                pure_components=self.pure_components,
-                               correction_model=linear_correction,
-                               rsme_threshold=0.1)
+                               correction_model=quadratic_correction)
 
     def test_no_x_axis_errors_should_pass(self) -> None:
         self.run_test(self.mixture_signal)
@@ -33,9 +35,23 @@ class TestEA(BaseTestCase):
         signal = interpolate_signal(self.mixture_signal, self.x, x_distorted, 0, 0)
         self.run_test(signal)
 
+    def test_quadratic_error_should_pass(self) -> None:
+        x_distorted = 0.01 * self.x ** 2 + 1.01 * self.x + 2
+        signal = interpolate_signal(self.mixture_signal, self.x, x_distorted, 0, 0)
+        self.run_test(signal)
+
     def test_speed(self):
         x_distorted = 1.01 * self.x - 2
         signal = interpolate_signal(self.mixture_signal, self.x, x_distorted, 0, 0)
         f = partial(self.solver.solve, signal=signal)
-        time = timeit.timeit(f, number=20)
-        self.assertLess(time, 10)
+        time = timeit.timeit(f, number=50)
+        self.assertLess(time, 1)
+
+    def test_with_random_concentrations_and_random_linear_x_errors_should_pass(self):
+        root_path = os.path.abspath(os.path.dirname(__file__))
+        file_path = os.path.join(root_path, 'data', 'mixtures.p')
+        mixture_data = load_pickle_file(file_path)
+        for item in mixture_data:
+            self.contributions = item['contributions']
+            signal = item['signal']
+            self.run_test(signal, decimal=1)
